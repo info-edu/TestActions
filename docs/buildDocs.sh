ac1dab35ba7f37cf661dee355a0a4aa72586bbd9
Amaury Dame <amaury@MacBookProAmo.local>
2020-12-01 13:11:13 +0100

    change install sphinx

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -17,8 +17,11 @@ set -x
 ###################
  
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme
- 
+#apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme
+apt-get -y install git rsync python3-pip 
+pip3 install -U sphinx
+pip3 install -U sphinx-rtd-theme
+
 #####################
 # DECLARE VARIABLES #
 #####################
